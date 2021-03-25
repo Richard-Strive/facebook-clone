e6d13f9201b090a16d1dc67d13ea87dbb02057e7
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-03-26 00:00:26 +0100

    Profile Page done

--- kairos-app/src/components/subcomponents/ProfilePagePost/ProfilePagePost.jsx
+++ kairos-app/src/components/subcomponents/ProfilePagePost/ProfilePagePost.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ProfilePagePost.css";
+import { Row, Col } from "react-bootstrap";
 import CreatePost from "../CreatePost/CreatePost";
 import DisplayedPost from "../DisplayedPost/DisplayedPost";
 
@@ -7,8 +8,60 @@ function ProfilePagePost() {
   return (
     <div className="profile_page_post_container">
       <div className="profile_page_post_left mt-2">
-        <div className="face_id_camera"></div>
-        <div className="friends_container mt-3"></div>
+        <div className="face_id_camera">
+          <img
+            src="https://specials-images.forbesimg.com/imageserve/5f76aa7a27e990f9cc618fe8/960x0.jpg?fit=scale"
+            alt="arificial_intelligence"
+          />
+          <h5>Enable Face ID login</h5>
+        </div>
+        <div className="friends_container mt-3 pt-3">
+          <b className="ml-3">Friends</b>
+          <div className="friends_container_row mt-4">
+            <Row className="mt-3">
+              <Col>
+                <img src="https://source.unsplash.com/random" alt="" />
+                <p>FriendName</p>
+              </Col>
+              <Col>
+                <img src="https://source.unsplash.com/random" alt="" />
+                <p>FriendName</p>
+              </Col>
+              <Col>
+                <img src="https://source.unsplash.com/random" alt="" />
+                <p>FriendName</p>
+              </Col>
+            </Row>
+            <Row className="mt-3">
+              <Col>
+                <img src="https://source.unsplash.com/random" alt="" />
+                <p>FriendName</p>
+              </Col>
+              <Col>
+                <img src="https://source.unsplash.com/random" alt="" />
+                <p>FriendName</p>
+              </Col>
+              <Col>
+                <img src="https://source.unsplash.com/random" alt="" />
+                <p>FriendName</p>
+              </Col>
+            </Row>
+            <Row className="mt-3">
+              <Col>
+                <img src="https://source.unsplash.com/random" alt="" />
+                <p>FriendName</p>
+              </Col>
+              <Col>
+                <img src="https://source.unsplash.com/random" alt="" />
+                <p>FriendName</p>
+              </Col>
+              <Col>
+                <img src="https://source.unsplash.com/random" alt="" />
+                <p>FriendName</p>
+              </Col>
+            </Row>
+          </div>
+        </div>
       </div>
       <div className="profile_page_post_right mt-2">
         <div className="profile_page_create_post">
