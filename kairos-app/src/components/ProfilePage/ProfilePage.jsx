9e7d232a6be7404be3e8a301ff611545c590dbbe
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-04-02 20:20:08 +0200

    added more style

--- kairos-app/src/components/ProfilePage/ProfilePage.jsx
+++ kairos-app/src/components/ProfilePage/ProfilePage.jsx
@@ -72,7 +72,7 @@ function ProfilePage({ setUserData }) {
           </div>
         </div>
         <h1 className="mt-3">{loading ? "...loading" : user.firstName}</h1>
-        <p>Add bio</p>
+        <p className="add_bio"> Add bio</p>
         <hr className="anti_flex_hr" />
         <div className="profile_page_header_bottom">
           <div className="profile_page_header_bottom_tabs">
