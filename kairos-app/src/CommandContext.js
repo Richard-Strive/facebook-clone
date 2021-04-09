35654d3e52fe1ec206345d0c56b16dc5cf94c623
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-04-09 15:47:13 +0200

    Testing some .env features

--- kairos-app/src/CommandContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const CommandContext = React.createContext(true);
-
-export const CommandProvider = CommandContext.Provider;
-export const CommandConsumer = CommandContext.Consumer;
-
-export default CommandContext;
